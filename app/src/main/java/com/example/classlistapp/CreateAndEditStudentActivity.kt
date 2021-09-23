9c7744f4c6577f011a553437d10de2a11e725590
David Svensson <davidsvson@gmail.com>
2021-09-23 11:17:35 +0200

    added new activity

---
+++ app/src/main/java/com/example/classlistapp/CreateAndEditStudentActivity.kt
@@ -0,0 +1,11 @@
+package com.example.classlistapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CreateAndEditStudentActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_and_edit_student)
+    }
+}
